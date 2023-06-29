90be87207d9b51075801799816c06c784372ed53
Joe Corrales <corralesjoe07@gmail.com>
2023-06-29 16:52:51 -0600

    detalles

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import Navbar from './Navbar';
 import './css/HomePage.css';
+import tailwindcss from '../assets/icons/tailwindcss.svg';
+import react from '../assets/icons/react.svg';
+import vite from '../assets/icons/vite.svg';
+import javascript from '../assets/icons/javascript.svg';
 
 const HomePage = () => {
     return (
         <>
-            <Navbar/>
+            <Navbar />
             <div>
-                <h1 className="title font-black text-pop-up-top text-gray-500 text-center md:mt-20 mt-80 sm:text-9xl text-6xl">
+                <h1 className="title font-black text-pop-up-top text-gray-500 text-center mt-80 sm:text-9xl text-6xl">
                     Trellux
                 </h1>
+            </div>
+            <div className="flex justify-center mt-10 gap-4">
+                <img src={javascript} alt="" className="w-10 h-10" />
+                <img src={react} alt="" className="w-10 h-10" />
+                <img src={vite} alt="" className="w-10 h-10" />
+                <img src={tailwindcss} alt="" className="w-10 h-10" />
             </div>
         </>
     );
