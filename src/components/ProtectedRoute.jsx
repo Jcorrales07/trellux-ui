63444cdd8c29ce4a47fd14173df409b122e1039a
Joe Corrales <corralesjoe07@gmail.com>
2023-07-12 19:31:17 -0600

    children ? children : <Outlet />

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 
-export const ProtectedRoute = ({ user, path }) => {
+export const ProtectedRoute = ({ children, user, path }) => {
     if (!user) {
         return <Navigate to={path} />
     }
-    return <Outlet />
+    return children ? children : <Outlet />
 }
 
 export default ProtectedRoute
